edfb4784a00c6e70ae9035616395cad4a501ffde
mr-spod <sean.odonnell@duethealth.com>
2018-05-03 20:01:38 -0400

    initial commit.

---
+++ Example/Pods/Target Support Files/Pods-VendoredFrameworkShell_Example/Pods-VendoredFrameworkShell_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VendoredFrameworkShell_Example : NSObject
+@end
+@implementation PodsDummy_Pods_VendoredFrameworkShell_Example
+@end
---
+++ Example/Pods/Target Support Files/VendoredFrameworkShell/VendoredFrameworkShell-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VendoredFrameworkShell : NSObject
+@end
+@implementation PodsDummy_VendoredFrameworkShell
+@end
